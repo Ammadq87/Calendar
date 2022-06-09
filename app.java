3f68d9a8b3076b2ffd5aa582e0266b904fa77d5b
Ammad <ammadq87@my.yorku.ca>
2022-06-09 01:00:44 -0400

    using this to run/test commands

---
+++ app.java
@@ -0,0 +1,38 @@
+
+package Calendar;
+
+import java.util.*;
+
+public class app {
+    public static void main(String args[]) {
+        new app();
+    }
+
+    public app() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Command. Type -h for help");
+        String cmd = input.nextLine();
+
+        commands c;
+
+        while (!cmd.equals("-q")) {
+            c = new commands(cmd);
+
+            if (!c.executeCommand(cmd)) {
+                if (!c.verifyCommand())
+                    System.out.println("Command <" + cmd + "> could not be executed");
+            }
+            print(c.getListOfEvents());
+            cmd = input.nextLine();
+        }
+
+    }
+
+    public void print(List<event> list) {
+        for (event e : list) {
+            System.out.println(e.eventToString());
+        }
+    }
+
+}
