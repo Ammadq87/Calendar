eecf1ed10b3e71d2ce17c8d5caf652611262e65a
Ammad <ammadq87@my.yorku.ca>
2022-06-12 02:25:27 -0400

    corrected the way to identify valid arguments

--- commands.java
+++ commands.java
@@ -1,4 +1,4 @@
-package Calendar;
+package JDBC;
 
 import java.util.*;
 
@@ -9,9 +9,15 @@ public class commands {
     String commandList[] = { "ce", "ls" };
     boolean printToScreen = false;
     String print = "";
+    static int id = 1;
     List<event> list = new ArrayList<event>();
 
     public commands(String command) {
+        this.command = command;
+        this.command = sanitizeCommand(command);
+    }
+
+    public void setCommand(String command) {
         this.command = command;
         this.command = sanitizeCommand(command);
     }
@@ -24,25 +30,116 @@ public class commands {
         return this.command.replaceAll("\\s{2,}", " ");
     }
 
+    public boolean verifyCommand() {
+        String text[] = command.split(" ");
+
+        if (!text[0].equalsIgnoreCase("event"))
+            return false;
+
+        for (int i = 1; i < text.length; i++) {
+            if (text[i].length() <= 1)
+                return false;
+
+            // Needs a better way of identifying commands
+            if (text[i].equalsIgnoreCase("ls") || text[i].equalsIgnoreCase("ce") || text[i].equalsIgnoreCase("-d")
+                    || text[i].equalsIgnoreCase("-t")) {
+                if (!boundsCheck(i, text))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean executeCommand(String command) {
+        if (!verifyCommand())
+            return false;
+
+        // Based on command, execute stuff, then return true
+        String text[] = command.split(" ");
+
+        for (int i = 1; i < text.length; i++) {
+            if (text[i].equalsIgnoreCase("ce")) {
+                String title = getArgument(text, i + 1);
+                String time = null;
+                String date = null;
+                for (int j = i + 2; j < text.length; j++) {
+                    if (text[j].equalsIgnoreCase("-d")) {
+                        date = text[j + 1];
+                    }
+
+                    if (text[j].equalsIgnoreCase("-t")) {
+                        time = text[j + 1];
+                    }
+                }
+                this.list.add(newEvent(title, date, time));
+                return true;
+            }
+
+            if (text[i].equalsIgnoreCase("ls")) {
+                this.printToScreen = true;
+                this.print += "Hi\n";
+            }
+
+        }
+        return false;
+    }
+
+    public event newEvent(String name, String date, String time) {
+        event e;
+        if (date == null && time == null) {
+            e = new event(name, id);
+        } else if (date == null) {
+            e = new event(name, time, id);
+        } else if (time == null) {
+            e = new event(name, date, id);
+        } else {
+            e = new event(name, date, time, id);
+        }
+        id++;
+        return e;
+    }
+
+    // Helper methods:
+    private String getArgument(String text[], int i) {
+        String joined = "";
+        for (int j = i; j < text.length; j++) {
+            if (isCommand(text[j]) || isFlag(text[j]))
+                break;
+            joined += text[j] + " ";
+        }
+
+        if (joined.charAt(joined.length() - 1) == ' ') {
+            joined = joined.substring(0, joined.length() - 1);
+        }
+
+        return joined == "" ? " " : joined;
+    }
+
     // Verifies argument after command
     private boolean verifyArgument(String text[], int i) {
         if (text[i].equalsIgnoreCase("ls") && i == text.length - 1) {
             return true;
         }
 
-        return (i < text.length - 1 && (text[i + 1].charAt(0) == text[i + 1].charAt(text[i + 1].length() - 1)
-                && text[i + 1].charAt(0) == '\''));
+        String argument = getArgument(text, i + 1);
+        if (argument.charAt(0) == '\'' && argument.charAt(argument.length() - 1) == '\'')
+            return true;
+        return false;
     }
 
     private boolean isCommand(String text) {
         if (text.length() >= 1) {
-            if (text.charAt(0) == '\'')
+            if (text.charAt(0) == '\'' || text.charAt(text.length() - 1) == '\'')
                 return false;
-            else if (text.charAt(0) == '-')
+            else if (isFlag(text))
                 return false;
         }
 
         return true;
+    }
+
+    private boolean isFlag(String text) {
+        return (text.charAt(0) == '-');
     }
 
     private boolean isDuplicateOrUnknownValue(String text[], int i) {
@@ -94,75 +191,6 @@ public class commands {
         }
 
         return true;
-    }
-
-    public boolean verifyCommand() {
-        String text[] = command.split(" ");
-
-        if (!text[0].equalsIgnoreCase("event"))
-            return false;
-
-        for (int i = 1; i < text.length; i++) {
-            if (text[i].length() <= 1)
-                return false;
-
-            // Needs a better way of identifying commands
-            if (text[i].equalsIgnoreCase("ls") || text[i].equalsIgnoreCase("ce") || text[i].equalsIgnoreCase("-d")
-                    || text[i].equalsIgnoreCase("-t")) {
-                if (!boundsCheck(i, text))
-                    return false;
-            }
-        }
-        return true;
-    }
-
-    public boolean executeCommand(String command) {
-        if (!verifyCommand())
-            return false;
-
-        // Based on command, execute stuff, then return true
-        String text[] = command.split(" ");
-
-        for (int i = 1; i < text.length; i++) {
-            if (text[i].equalsIgnoreCase("ce")) {
-                String title = text[i + 1];
-                String time = null;
-                String date = null;
-                for (int j = i + 2; j < text.length; j++) {
-                    if (text[j].equalsIgnoreCase("-d")) {
-                        date = text[j + 1];
-                    }
-
-                    if (text[j].equalsIgnoreCase("-t")) {
-                        time = text[j + 1];
-                    }
-                }
-                this.list.add(newEvent(title, date, time));
-                return true;
-            }
-
-            if (text[i].equalsIgnoreCase("ls")) {
-                this.printToScreen = true;
-                this.print += "Hi\n";
-            }
-
-        }
-        return false;
-    }
-
-    public event newEvent(String name, String date, String time) {
-        event e;
-        if (date == null && time == null) {
-            e = new event(name);
-        } else if (date == null) {
-            e = new event(name, time);
-        } else if (time == null) {
-            e = new event(name, date);
-        } else {
-            e = new event(name, date, time);
-        }
-
-        return e;
     }
 
 }
