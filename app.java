aa8ecf6cd93a4c61cb10eeda82abbe4b33cb6a7e
Ammadq87 <77247515+Ammadq87@users.noreply.github.com>
2022-06-18 01:49:45 -0400

    Cleaned up code
    Removed DB access and refactored the execution

--- app.java
+++ app.java
@@ -1,78 +1,36 @@
-package Calendar;
+package MainApp;
 
 import java.util.*;
 import java.sql.*;
 
 public class app {
 
-    public static void main(String args[]) {
-        new app();
-    }
+	public static void main(String args[]) {
+		new app();
+	}
 
-    Connection connection = null;
+	public Connection con = null;
 
-    public app() {
-        connectToDB();
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Command. Type -h for help");
-        String cmd = input.nextLine();
+	public app() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Command. Type -h for help");
+		String cmd = input.nextLine();
 
-        commands c = new commands(cmd);
+		Command c = new Command(cmd);
 
-        while (!cmd.equals("-q")) {
-            if (!c.executeCommand(cmd)) {
-                if (!c.verifyCommand())
-                    System.out.println("Command <" + cmd + "> could not be executed");
-            }
+		while (!cmd.equals("-q")) {
+			if (!c.isCommandValid()) {
+				System.out.println("<error: "+c.errorMessage+" >");
+			} else {
+				c.executeCommand();	
+			}
+			
+			cmd = input.nextLine();
+			c.setCommand(cmd);
+		}
 
-            if (c.printToScreen) {
-                System.out.println(c.print);
-                c.printToScreen = false;
-            }
+		input.close();
 
-            print(c.getListOfEvents());
-            cmd = input.nextLine();
-            c.setCommand(cmd);
-        }
-
-        input.close();
-
-    }
-
-    public void print(List<event> list) {
-        for (event e : list) {
-            System.out.println(e.eventToString());
-        }
-    }
-
-    public void connectToDB() {
-        Connection con = null;
-
-        try {
-            String url = "jdbc:mysql://localhost:3306/sys";
-            String username = "root";
-            String password = "Ammadq87";
-
-            // Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection(url, username, password);
-
-            if (con != null) {
-                System.out.println("Successfully connected to MySQL database test");
-                Statement stmnt = con.createStatement();
-                ResultSet r = stmnt.executeQuery("select * from students");
-                while (r.next()) {
-                    String data = "";
-                    for (int i = 1; i <= 5; i++) {
-                        data += r.getString(i) + ":";
-                    }
-                    System.out.println(data);
-                }
-            }
-
-        } catch (SQLException ex) {
-            System.out.println("An error occurred while connecting MySQL databse");
-            ex.printStackTrace();
-        }
-    }
+	}
 
 }
