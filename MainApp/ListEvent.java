b2132c67c188bb4e7b93fa138dc5f67b233d155e
Ammad <ammadq87@my.yorku.ca>
2022-07-27 01:29:26 -0400

    Implemented display feature for calendar;

--- MainApp/ListEvent.java
+++ MainApp/ListEvent.java
@@ -39,20 +39,65 @@ public class ListEvent extends Command {
      */
 
     Event e = new Event();
+    String schedule[][] = new String[96][2];
 
     public ListEvent(String parameters[]) {
         this.parameters = parameters;
+        initializeScheduleFormat();
     }
 
     public void execute() {
         List<String[]> l = super.getResultsFromQuery(getQuery(parameters, "events", null), "name-s", "startTime-i",
                 "endTime-i", "eventDate-s");
+
         for (String s[] : l) {
-            for (int i = 0; i < s.length; i++) {
-                System.out.print(s[i] + " | ");
+
+            int start = (Integer.parseInt(s[1]) % 100) / 15 + (Integer.parseInt(s[1]) / 100) * 4;
+            int end = (Integer.parseInt(s[2]) % 100) / 15 + (Integer.parseInt(s[2]) / 100) * 4;
+
+            for (int i = start; i <= end; i++) {
+                if (i == start) {
+                    this.schedule[i][1] = this.schedule[i][1] != null ? this.schedule[i][1] + " CONFLICTS WITH " + s[0]
+                            : s[0];
+                } else if (i != start && i != end) {
+                    this.schedule[i][1] = this.schedule[i][1] != null ? this.schedule[i][1] + " CONFLICTS WITH " + s[0]
+                            : formatTitle('|', s[0].length());
+                } else if (i == end) {
+                    this.schedule[i][1] = this.schedule[i][1] != null ? this.schedule[i][1] + " CONFLICTS WITH " + s[0]
+                            : formatTitle('V', s[0].length());
+                }
             }
-            System.out.println("");
         }
+        printSchedule();
+    }
+
+    private void initializeScheduleFormat() {
+        int hour = -1;
+        for (int i = 0; i < this.schedule.length; i++) {
+            if ((i * 15) % 60 == 0) {
+                hour += 1;
+                this.schedule[i][0] = "[" + (hour < 10 ? "0" + hour : "" + hour) + ":00]";
+            } else {
+
+                this.schedule[i][0] = "[" + (hour < 10 ? "0" + hour : "" + hour) + ":" + (i * 15) % 60 + "]";
+            }
+        }
+    }
+
+    public void printSchedule() {
+        for (int i = 0; i < this.schedule.length; i++) {
+            String output = this.schedule[i][0] + " " + (this.schedule[i][1] == null ? " " : this.schedule[i][1]);
+            System.out.println(output);
+        }
+    }
+
+    public String formatTitle(char n, int spc) {
+        String output = "";
+        for (int i = 0; i < spc / 2; i++) {
+            output += " ";
+        }
+        output += n;
+        return output;
     }
 
     /*
@@ -70,6 +115,8 @@ public class ListEvent extends Command {
             query += " WHERE month = " + this.e._date[0] + " AND day = " + this.e._date[1] + " AND year = "
                     + this.e._date[2];
         }
+
+        // Add Cases for date and time
 
         return query + ";";
     }
