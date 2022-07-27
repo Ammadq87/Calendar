3777e9ae78e745411e326ca03d447567e4944cd9
Ammad <ammadq87@my.yorku.ca>
2022-07-27 01:28:39 -0400

    Cleaned up code

--- MainApp/FindEvent.java
+++ MainApp/FindEvent.java
@@ -1,6 +1,8 @@
 package MainApp;
 
-public class FindEvent {
+import java.util.List;
+
+public class FindEvent extends Command {
 
     String parameters[];
 
@@ -9,7 +11,33 @@ public class FindEvent {
     }
 
     public void execute() {
+        List<String[]> l = super.getResultsFromQuery(createQuery("events"), "name-s", "startTime-i", "endTime-i",
+                "eventDate-s");
+        for (String s[] : l) {
+            for (int i = 0; i < s.length; i++) {
 
+                switch (i) {
+                    case 0:
+                        System.out.print(">> " + (i + 1) + ". " + s[i]);
+                        break;
+                    case 1:
+                        System.out
+                                .print(" occuring at " + s[i] + "" + (Integer.parseInt(s[i]) < 12 ? "am" : "pm") + "-");
+                        break;
+                    case 2:
+                        System.out.print(s[i] + "" + (Integer.parseInt(s[i]) < 12 ? "am" : "pm"));
+                        break;
+                    case 3:
+                        System.out.print(" on " + s[i]);
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    public String createQuery(String table) {
+        String query = "SELECT * FROM " + table + " WHERE name = \"" + parameters[0] + "\"";
+        return query;
     }
 
 }
